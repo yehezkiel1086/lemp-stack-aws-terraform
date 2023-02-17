12b4de125062035a2e87e5af67f98fe99231d357
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2023-02-17 19:37:06 +0700

    add completed main.tf script

--- main.tf
+++ main.tf
@@ -1,135 +1,157 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.54.0"
-    }
-  }
-}
-
+# AWS Provider config
 provider "aws" {
-  # Configuration options
   region = "us-east-1"
-
+  access_key = ""
+  secret_key = ""
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-0557a15b87f6559cf"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "AppServerExample"
-  }
-
-}
-
-resource "aws_vpc" "main" {
+# 1. Buat VPC
+resource "aws_vpc" "prod-vpc" {
   cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
-
   tags = {
-    Name = "main"
+    Name = "production"
   }
 }
 
-resource "aws_route_table" "rt" {
-  vpc_id = aws_vpc.main.id
+# 2. Buat gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.prod-vpc.id
+}
+
+# 3. Buat route table
+resource "aws_route_table" "prod-route-table" {
+  vpc_id = aws_vpc.prod-vpc.id
 
   route {
-    cidr_block = "10.0.1.0/24"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
   }
 
   route {
-    ipv6_cidr_block        = "::/0"
-    egress_only_gateway_id = aws_egress_only_internet_gateway.gw.id
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.gw.id
   }
 
   tags = {
-    Name = "main"
+    Name = "Prod"
   }
 }
 
-resource "aws_subnet" "as" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+# 4. Buat subnet
+resource "aws_subnet" "subnet-1" {
+  vpc_id            = aws_vpc.prod-vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
 
   tags = {
-    Name = "Main"
+    Name = "prod-subnet"
   }
 }
 
+# 5. Hubungkan subnet dengan route table
 resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.as.id
-  route_table_id = aws_route_table.rt.id
+  subnet_id      = aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.prod-route-table.id
 }
 
-
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    description      = "https"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.main.cidr_block]
-    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
-  }
+# 6. Buat firewall sehingga hanya bisa mengakses port 22, 80, dan 443
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web_traffic"
+  description = "Allow Web inbound traffic"
+  vpc_id      = aws_vpc.prod-vpc.id
 
   ingress {
-    description      = "SSH"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.main.cidr_block]
-    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-  
   ingress {
-    description      = "http"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.main.cidr_block]
-    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-
-
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
-    Name = "allow_tls"
+    Name = "allow_web"
   }
 }
 
-resource "aws_network_interface" "test" {
-  subnet_id       = aws_subnet.as.id
-  private_ips     = ["10.0.0.50"]
-  security_groups = [aws_security_group.allow_tls.id]
-
-  attachment {
-    instance     = aws_instance.app_server.id
-    device_index = 1
-  }
+# 7. Buat sebuah network interface dengan ip pada subnet yang telah dibuat pada langkah 4
+resource "aws_network_interface" "web-server-nic" {
+  subnet_id       = aws_subnet.subnet-1.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.allow_web.id]
 }
 
+# 8. Buatlah sebuah ip elastis pada network interface yang telah dibuat pada langkah 7
 resource "aws_eip" "one" {
   vpc                       = true
-  network_interface         = aws_network_interface.test.id
-  associate_with_private_ip = "10.0.0.50"
-  depends_on = aws_internet_gateway.gw
-  
+  network_interface         = aws_network_interface.web-server-nic.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on                = [aws_internet_gateway.gw]
+}
+
+output "server_public_ip" {
+  value = aws_eip.one.public_ip
+}
+
+# 9. Buatlah server Ubuntu dan install nginx php mysql composer serta tools lainnya 
+# yang dibutuhkan untuk melakukan deploy aplikasi laravel
+resource "aws_instance" "web-server-instance" {
+  ami               = "ami-0557a15b87f6559cf"
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-1a"
+  key_name          = ""
+
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.web-server-nic.id
+  }
+
+  user_data = <<-EOF
+                #!/bin/bash
+                # install php
+                sudo add-apt-repository ppa:ondrej/php
+                sudo apt-get install -y php7.4-{cli,fpm,mbstring,mysql,curl,mcrypt,xml}	
+
+                # install mysql
+                sudo apt-get install -y mysql-server
+
+                # install nginx
+                sudo apt install -y nginx
+
+                # install git
+                sudo apt-get install -y git
+
+                # install zip & unzip
+                sudo apt-get install -y zip unzip
+
+                # install composer
+                php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+                php -r "if (hash_file('sha384', 'composer-setup.php') === '55ce33d7678c5a611085589f1f3ddf8b3c52d662cd01d4ba75c0ee0459970c2200a51f492d557530c71c15d8dba01eae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+                php composer-setup.php
+                php -r "unlink('composer-setup.php');"
+                sudo mv composer.phar /usr/local/bin/composer
+                EOF
+  tags = {
+    Name = "linux-ubuntu"
+  }
 }
